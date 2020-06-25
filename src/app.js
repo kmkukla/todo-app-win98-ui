8740046ddcacde3c55714025d4c46147a9db1f2e
Konrad Kukla <kondziocard@gmail.com>
2020-06-25 20:25:16 +0200

    add edit option

--- src/app.js
+++ src/app.js
@@ -1,22 +1,39 @@
 const addBtn = document.getElementById("add-btn");
 const cancelBtn = document.getElementById("cancel-btn");
+const editBtn = document.getElementById("edit-btn");
 const deleteBtn = document.getElementById("delete-btn");
 const deleteAllBtn = document.getElementById("delete-all-btn");
 const input = document.getElementById("input");
 const listContainer = document.querySelector(".tree-view");
 let listItems = document.querySelectorAll("li");
+const form = document.querySelector("form");
 
-addBtn.addEventListener("click", addItem);
+let editElement;
+let editFlag = false;
+
+form.addEventListener("submit", addItem);
 cancelBtn.addEventListener("click", cancelAction);
+editBtn.addEventListener("click", editItem);
 deleteBtn.addEventListener("click", deleteItem);
 deleteAllBtn.addEventListener("click", deleteAllItems);
 
 // functions
 function addItem() {
-  if (input.value) {
+  event.preventDefault();
+  // check if an input contains only whitespace
+  if (!input.value.replace(/\s/g, "").length) {
+    return;
+  }
+
+  if (input.value && !editFlag) {
     const item = document.createElement("li");
     item.textContent = input.value;
     listContainer.appendChild(item);
+    input.value = "";
+  } else if (input.value && editFlag) {
+    editElement.textContent = input.value;
+    addBtn.textContent = "ADD";
+    editFlag = false;
     input.value = "";
   }
   listItems = document.querySelectorAll("li");
@@ -28,17 +45,32 @@ function addItem() {
   );
 }
 
-function deleteItem() {
+function editItem() {
   listItems.forEach((item) => {
-    item.classList.contains("selected")
-      ? item.parentNode.removeChild(item)
-      : null;
+    if (item.classList.contains("selected")) {
+      editElement = item;
+      input.value = editElement.innerHTML;
+      editFlag = true;
+      addBtn.textContent = "Edit";
+    }
   });
+}
+
+function deleteItem() {
+  if (listContainer.children.length > 0) {
+    listItems.forEach((item) => {
+      item.classList.contains("selected")
+        ? item.parentNode.removeChild(item)
+        : null;
+    });
+  }
 }
 
 function cancelAction() {
   input.value ? (input.value = "") : null;
   listItems.forEach((item) => item.classList.remove("selected"));
+  editFlag = false;
+  addBtn.textContent = "ADD";
 }
 
 function deleteAllItems() {
